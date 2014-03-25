901ab6c581982c2f797eee19f7e8a9cb98b289ab
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-03-25 13:37:08 -0600

    Implemented game rules

--- project_code/app.js
+++ project_code/app.js
@@ -5,17 +5,47 @@ goog.require('app.Board');
 
 goog.require('goog.dom');
 
-var main = function () {
+var main = function() {
     var SIZE = {
-        width: 700,
-        height: 450
+        width: 140,
+        height: 100
     };
 
+    var CELL_SIZE = {
+        width: 5,
+        height: 5
+    };
+
+    var colors = {
+        DEAD: 'rgba(10, 10, 10, 0.0)',
+        LIVE: 'rgba(75, 10, 10, 0.05)',
+        GONE: 'rgba(50, 50, 50, 0.005)'
+    };
+
+    var iter = 0;
+    var MAX_ITER = 10000;
+
     var canvas = new app.Canvas(SIZE.width, SIZE.height);
-    var board = new app.Board(canvas);
+    var board = new app.Board(canvas, colors, CELL_SIZE);
+
+    canvas.element.width = SIZE.width * CELL_SIZE.width;
+    canvas.element.height = SIZE.height * CELL_SIZE.height;
 
     canvas.bindTo(goog.dom.getElement('screen'));
-    board.render();
+
+    function up() {
+        iter++;
+        board.update();
+        if (iter > MAX_ITER) {
+            canvas.clear();
+            iter = 0;
+        }
+
+        board.render();
+        setTimeout(up, 75);
+    }
+
+    up();
 };
 
 goog.exportSymbol('main', main);
--- project_code/src/app/Board.js
+++ project_code/src/app/Board.js
@@ -58,20 +58,130 @@ app.Board.Palette;
 /** @typedef {{width: number, height: number}} */
 app.Board.CellSize
 
+/** @typedef {{x: number, y: number}} */
+app.Board.Position
+
 app.Board.prototype.init = function() {
     for (var i = 0, len = this.size.width * this.size.height; i < len; i++) {
-        this.grid[i] = new app.Cell();
+        this.grid[i] = new app.Cell(Math.random() * 100 > 75);
     }
 };
 
+/**
+ *
+ */
 app.Board.prototype.update = function() {
+    var pos = {};
+    var neighbors = 0;
+
+    for (var i = 0, len = this.size.width * this.size.height; i < len; i++) {
+        pos = this.getXY(i);
+        neighbors = this.countNeighbors(pos.x, pos.y);
+
+        if (this.grid[i].state.curr) {
+            if (neighbors != 2 && neighbors != 3) {
+                this.grid[i].state.next = 0;
+            }
+        } else {
+            if (neighbors === 3) {
+                this.grid[i].state.next = 1;
+            }
+        }
+    }
+
+    for (var i = 0, len = this.size.width * this.size.height; i < len; i++) {
+        this.grid[i].state.last = this.grid[i].state.curr;
+        this.grid[i].state.curr = this.grid[i].state.next;
+    }
 };
 
+/**
+ *
+ */
 app.Board.prototype.render = function() {
+    var pos = {};
+    var color = '';
+
+    for (var i = 0, len = this.size.width * this.size.height; i < len; i++) {
+        pos = this.getXY(i);
+        color = this.grid[i].state.curr ? this.colors.LIVE : this.colors.DEAD;
+        this.canvas.draw(pos.x * this.cellSize.width, pos.y * this.cellSize.height, this.cellSize.width, this.cellSize.height, color);
+
+        color = this.grid[i].state.last ? this.colors.GONE : this.colors.DEAD;
+        this.canvas.draw(pos.x * this.cellSize.width, pos.y * this.cellSize.height, this.cellSize.width, this.cellSize.height, color);
+    }
 };
 
+/**
+ *
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
 app.Board.prototype.countNeighbors = function(x, y) {
+    var total = 0;
+    var dir = {
+        up: y > 0,
+        down: y < this.size.height - 1,
+        left: x > 0,
+        right: x < this.size.width - 1
+    };
+
+
+    if (dir.up) {
+        total += this.cellAt(x, y - 1).state.curr;
+    }
+
+    if (dir.down) {
+        total += this.cellAt(x, y + 1).state.curr;
+    }
+
+    if (dir.left) {
+        total += this.cellAt(x - 1, y).state.curr;
+    }
+
+    if (dir.right) {
+        total += this.cellAt(x + 1, y).state.curr;
+    }
+    if (dir.up && dir.left) {
+        total += this.cellAt(x - 1, y - 1).state.curr;
+    }
+
+    if (dir.up && dir.right) {
+        total += this.cellAt(x + 1, y - 1).state.curr;
+    }
+
+    if (dir.down && dir.left) {
+        total += this.cellAt(x - 1, y + 1).state.curr;
+    }
+
+    if (dir.down && dir.right) {
+        total += this.cellAt(x + 1, y + 1).state.curr;
+    }
+
+    return total;
 };
 
+/**
+ *
+ * @param {number} i
+ * @returns {app.Board.Position}
+ */
 app.Board.prototype.getXY = function(i) {
+    var x = i % this.size.width;
+
+    return {
+        x: x,
+        y: (i - x) / this.size.width
+    };
+};
+
+/**
+ *
+ * @param {number} x
+ * @param {number} y
+ * @returns {app.Cell}
+ */
+app.Board.prototype.cellAt = function(x, y) {
+    return this.grid[this.size.width * y + x];
 };
